db633feaf1d91fa63a6ddc17ed661cf09042ddd2
Maghfieggy <eggydj@gmail.com>
2019-03-17 15:13:45 +0700

    update baru kelompok hewan

--- kelompokhewan.js
+++ kelompokhewan.js
@@ -1,28 +1,30 @@
 function groupAnimals(animals) {
   // you can only write your code here!
   
-var i = 0;
-   var check = [];
+
+   var arr1 = [];
    var output1 = [];
    var output2 = [];
    var output3 = [];
    var output4 = [];
    var result1 = [];
-   var l = animals.length;
-   while (i < l){
-       if (output1.length == check.length || output1[0][0] == animals[i][0]){
+   var j = animals.length;
+   
+   for (var i = 0; i<j ;i++) {
+
+       if (output1.length == arr1.length || output1[0][0] == animals[i][0]){
            output1.push(animals[i]);
        }
-       else if (output2.length == check.length || animals[i][0] == output2[0][0]){
+       else if (output2.length == arr1.length || animals[i][0] == output2[0][0]){
            output2.push(animals[i]);
        }
-       else if (output3.length == check.length || animals[i][0] == output3[0][0]){
+       else if (output3.length == arr1.length || animals[i][0] == output3[0][0]){
            output3.push(animals[i]);
        }
-       else if (output4.length == check.length || animals[i][0] == output4[0][0]){
+       else if (output4.length == arr1.length || animals[i][0] == output4[0][0]){
            output4.push(animals[i]);
        }
-   i++;
+ 
    }
    result1.push(output2);
    result1.push(output1);
