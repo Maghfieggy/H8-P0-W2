f1ca1ea7e5614a01b00f5be4cf4b9913151bd506
Maghfieggy <eggydj@gmail.com>
2019-03-17 15:00:09 +0700

    update kelompok hewan

--- kelompokhewan.js
+++ kelompokhewan.js
@@ -1,28 +1,37 @@
 function groupAnimals(animals) {
   // you can only write your code here!
   
-var urut = animals.sort();
-var hasil = animals[0][0];
-var a = [];
-var b = [];
-
-for (var i = 0; i < urut.length; i++ ){
-	if (hasil === animals[i][0]){
-		a.push(animals[i]);
-	} else {
-		b.push(a);
-		a = [];
-		hasil = animals[i][0];
-		a.push(animals[i]);
-	}
-	if (i === animals.length-1) {
-		b.push(a);
-	}
-}
-
-return b;
-
-}
+var i = 0;
+   var check = [];
+   var output1 = [];
+   var output2 = [];
+   var output3 = [];
+   var output4 = [];
+   var result1 = [];
+   var l = animals.length;
+   while (i < l){
+       if (output1.length == check.length || output1[0][0] == animals[i][0]){
+           output1.push(animals[i]);
+       }
+       else if (output2.length == check.length || animals[i][0] == output2[0][0]){
+           output2.push(animals[i]);
+       }
+       else if (output3.length == check.length || animals[i][0] == output3[0][0]){
+           output3.push(animals[i]);
+       }
+       else if (output4.length == check.length || animals[i][0] == output4[0][0]){
+           output4.push(animals[i]);
+       }
+   i++;
+   }
+   result1.push(output2);
+   result1.push(output1);
+   result1.push(output3);
+   if (output4.length >= 1){
+   result1.push(output4);
+   }
+   return result1;
+ }
 
 // TEST CASES
 console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
